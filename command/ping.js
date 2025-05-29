f6b768ecb00b57d3046c363291359dd732ee8811
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2025-05-29 11:09:53 +0100

    Update ping.js

--- command/ping.js
+++ command/ping.js
@@ -1,1 +1,13 @@
+const config = {
+  name: "ping",
+  aliases: ["p"],
+  version: "1.0",
+  author: "heisbroken",
+  shortDescription: { en: "Ping the server." },
+};
 
+const onStart = async ({ message }) => {
+  message.reply("Pong! 🏓");
+};
+
+module.exports = { config, onStart };
